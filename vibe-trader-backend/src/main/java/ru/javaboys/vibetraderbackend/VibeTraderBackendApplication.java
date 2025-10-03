bbc0fcb9b7400bab3d142f3beea991f9c101bede
RustamKuramshin <leo.in.empty@gmail.com>
2025-10-03 22:12:53 +0300

    git init

---
+++ vibe-trader-backend/src/main/java/ru/javaboys/vibetraderbackend/VibeTraderBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.javaboys.vibetraderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeTraderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeTraderBackendApplication.class, args);
+	}
+
+}
