7aa92a469e8b19b3d496aa42e9a3c93a69c4dee1
Alex Ianchii <a.ianchii@mail.ru>
2025-10-04 14:29:02 +0300

    Фикс багов

--- vibe-trader-backend/src/main/java/ru/javaboys/vibetraderbackend/finam/client/FinamClientAuthInterceptor.java
+++ vibe-trader-backend/src/main/java/ru/javaboys/vibetraderbackend/finam/client/FinamClientAuthInterceptor.java
@@ -6,12 +6,14 @@ import java.time.temporal.ChronoUnit;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ru.javaboys.vibetraderbackend.finam.client.api.SessionsApiV1;
 import ru.javaboys.vibetraderbackend.finam.dto.auth.AuthRequest;
 import ru.javaboys.vibetraderbackend.finam.dto.auth.AuthResponse;
 import ru.javaboys.vibetraderbackend.finam.dto.auth.TokenDetailsRequest;
 import ru.javaboys.vibetraderbackend.finam.dto.auth.TokenDetailsResponse;
 
+@Slf4j
 @RequiredArgsConstructor
 public class FinamClientAuthInterceptor implements RequestInterceptor {
     private final SessionsApiV1 SessionsApi;
@@ -26,12 +28,14 @@ public class FinamClientAuthInterceptor implements RequestInterceptor {
     }
 
     private TokenInfo getTokenInfo() {
-        if (tokenInfo == null || tokenInfo.getExpiresAt().isAfter(Instant.now().plus(3, ChronoUnit.MINUTES))) {
+        if (tokenInfo == null || tokenInfo.getExpiresAt().minus(3, ChronoUnit.MINUTES).isAfter(Instant.now())) {
+            log.info("Requesting new token, old tokenInfo: {}", tokenInfo);
             AuthResponse token = SessionsApi.auth(AuthRequest.builder().secret(secret).build());
             TokenDetailsResponse tokenResponse = SessionsApi.tokenDetails(TokenDetailsRequest.builder().token(token.getToken()).build());
             tokenInfo = new TokenInfo(token.getToken(), tokenResponse.getExpiresAt());
         }
 
+        log.info("Using current token, tokenInfo: {}", tokenInfo);
         return tokenInfo;
     }
 
